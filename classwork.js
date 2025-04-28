cff34bce2f493650ca5b605c953a23c1f1a323eb
Mawunepey <fredimah16@gmail.com>
2025-04-28 15:17:27 -0700

    classwork

---
+++ classwork.js
@@ -0,0 +1,14 @@
+
+
+let score = 88;
+if (score >= 90)
+    {console.log("Grade A");}
+else if(score >=80)
+    {console.log("Grade B");}
+else if(score >=70)
+    {console.log("Grade c");}
+else if(score <70)
+{console.log("Grade F");}
+
+
+
